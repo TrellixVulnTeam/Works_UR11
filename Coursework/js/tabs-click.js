109282015c3e9dfa0a6cebe11671cc2a690e1211
CrtdLunacy <72741793+CrtdLunacy@users.noreply.github.com>
2022-07-28 21:04:19 +0400

    Delete Coursework directory

--- Coursework/js/tabs-click.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $(".catalog__countryItem").click(function(e) {
-        e.preventDefault(); /* To prevent redirect of <a> */
-        $(".catalog__countryItem").removeClass("aCountry"); /* Remove the class to all nav-items */
-        $(this).addClass("aCountry"); /* Add the class to clicked nav-items*/
-    });
-});
